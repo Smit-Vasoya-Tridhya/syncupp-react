095bfa2ec0f9f5ff921c027176d4b9f53677d415
Abhay <abhayktridhyatech@gmail.com>
2024-01-29 14:34:41 +0530

    - invoice API changes and also change in add new invoice UI changes

--- src/api/user/invoice/invoiceApis.tsx
+++ src/api/user/invoice/invoiceApis.tsx
@@ -1,16 +1,33 @@
 import AxiosDefault from "../../../services/AxiosDefault";
 
-type PostAddTeamMemberApiData = {
+type PostAddInvoiceApiData = {
     email: string;
     name: string;
     contact_number?: string;
     role?: string;
   }
+type GetInvoiceApiData = {
+  _id: string;
+  company_name: string;
+  name: string;
+  first_name?: string;
+  last_name?: string;
+  }
 
-  export const PostAddTeamMemberApi = async (data: PostAddTeamMemberApiData) => {
+  export const PostAddInvoiceApi = async (data: PostAddInvoiceApiData) => {
     const response = await AxiosDefault({
       url: "/api/v1/team-member/add",
       method: "POST",
+      data: data,
+      contentType: "application/json", 
+    });
+    const responseData = response.data;
+    return responseData;
+  };
+  export const GetInvoiceApi = async (data: GetInvoiceApiData) => {
+    const response = await AxiosDefault({
+      url: "/api/v1/invoice/get-clients",
+      method: "GET",
       data: data,
       contentType: "application/json", 
     });
