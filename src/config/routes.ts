c784c5fe98adf5c894ec9cddc7b45bd7682617b4
BhautikZ <bhautikztridhyatech@gmail.com>
2024-02-07 18:24:12 +0530

    -coupon management add update and delete & list

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,5 +1,4 @@
 export const routes = {
-
   // User panel
 
   home: '/home',
@@ -7,26 +6,26 @@ export const routes = {
   signUp: '/signup',
   forgotPassword: '/forgot-password',
   resetPassword: '/reset-password',
-  viewProfile:'/view-profile',
+  viewProfile: '/view-profile',
   dashboard: '/dashboard',
   client: '/client',
   team: '/team',
   agency_team: '/agency-team',
   client_team: '/client-team',
-  invoice:'/invoice',
-  invoiceForm:'/invoice/create-edit',
-  invoiceView:'/invoice/view-invoice',
-  viewTeam:'/team/view-team',
+  invoice: '/invoice',
+  invoiceForm: '/invoice/create-edit',
+  invoiceView: '/invoice/view-invoice',
+  viewTeam: '/team/view-team',
   task: '/task',
   UserCalender: '/calender',
   UserCalenderForm: '/calender/calender-form',
   UserCalenderAddActivity: '/calender/create-edit',
 
   teams: {
-    verify: '/team/verify'
+    verify: '/team/verify',
   },
   clients: {
-    verify: '/client/verify'
+    verify: '/client/verify',
   },
   agreement: '/agreement',
 
@@ -34,17 +33,19 @@ export const routes = {
   admin: {
     signIn: '/admin/signin',
     signUp: '/admin/signup',
-    forgotPassword:'/admin/forgot-password',
-    resetPassword:'/admin/reset-password',
+    forgotPassword: '/admin/forgot-password',
+    resetPassword: '/admin/reset-password',
     dashboard: '/admin/dashboard',
     viewProfile: '/admin/profile',
-    faq:'/admin/faq',
-    clientReview:'/admin/client-review',
-    agencylist:"/admin/agencylist",
-    inquiry:"/admin/inquiry",
-    payment:"/admin/payment-transaction",
-    cms:'/admin/cms',
-    cmsTC:'/admin/cms/team-and-condition',
+    faq: '/admin/faq',
+    clientReview: '/admin/client-review',
+    agencylist: '/admin/agencylist',
+    inquiry: '/admin/inquiry',
+    payment: '/admin/payment-transaction',
+    cms: '/admin/cms',
+    cmsTC: '/admin/cms/team-and-condition',
+    couponManagement: '/admin/coupon-management',
+    createCouponManagement: `/admin/coupon-management/create`,
+    UpdateCouponManagement: `/admin/coupon-management/update`,
   },
-
 };
