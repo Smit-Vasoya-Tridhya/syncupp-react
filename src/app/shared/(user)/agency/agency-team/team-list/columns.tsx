5da47bad8b1f29a78ccf74aa680bd725298a758b
HarshalGhadiya <harshalgtridhyatech@gmail.com>
2024-02-07 15:42:45 +0530

    Add payment for the Agency team

--- src/app/shared/(user)/agency/agency-team/team-list/columns.tsx
+++ src/app/shared/(user)/agency/agency-team/team-list/columns.tsx
@@ -2,18 +2,23 @@
 
 import Link from 'next/link';
 import { HeaderCell } from '@/components/ui/table';
-import {Text } from '@/components/ui/text';
+import { Text } from '@/components/ui/text';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Tooltip } from '@/components/ui/tooltip';
 import { routes } from '@/config/routes';
 import EyeIcon from '@/components/icons/eye';
 import PencilIcon from '@/components/icons/pencil';
-import {TeamMemberType } from '@/data/products-data';
+import { TeamMemberType } from '@/data/products-data';
 import DeletePopover from '@/app/shared/delete-popover';
 import CustomModalButton from '@/app/shared/custom-modal-button';
 import AddTeamMemberForm from '../create-edit/add-team-member-form';
 import { Badge, Button } from 'rizzui';
 import moment from 'moment';
+import { useRouter } from 'next/navigation';
+import { useDispatch, useSelector } from 'react-redux';
+import { initiateRazorpay } from '@/services/clientpaymentService';
+import { getAllTeamMember } from '@/redux/slices/user/team-member/teamSlice';
+
 
 type Columns = {
   data: any[];
@@ -71,7 +76,7 @@ function getRoleName(role: string) {
   }
 }
 
-export const getColumns = ({
+export const GetclientTeamColumns = ({
   data,
   sortConfig,
   checkedItems,
@@ -82,166 +87,187 @@ export const getColumns = ({
   currentPage,
   pageSize,
   searchTerm
-}: Columns) => [
-  {
-    title: (
-      <div className="ps-3.5">
-        <Checkbox
-          title={'Select All'}
-          onChange={handleSelectAll}
-          checked={checkedItems.length === data.length}
-          className="cursor-pointer"
-        />
-      </div>
-    ),
-    dataIndex: 'checked',
-    key: 'checked',
-    width: 50,
-    render: (_: any, row: any) => (
-      <div className="inline-flex ps-3.5">
-        <Checkbox
-          className="cursor-pointer"
-          checked={checkedItems.includes(row._id)}
-          {...(onChecked && { onChange: () => onChecked(row._id) })}
-        />
-      </div>
-    ),
-  },
-  {
-    title: (
-    <HeaderCell
-    title="Name"
-    sortable
-    ascending={
-      sortConfig?.direction === 'asc' && sortConfig?.key === 'name'
-    }
-    />),
-    onHeaderCell: () => onHeaderCellClick('name'),
-    dataIndex: 'name',
-    key: 'name',
-    width: 200,
-    render: (value: string) => (
-      <Text className="font-medium text-gray-700">{value}</Text>
-    ),
-  },
-  {
-    title: (
-    <HeaderCell
-    title="Mobile Number"
-    sortable
-    ascending={
-      sortConfig?.direction === 'asc' && sortConfig?.key === 'contact_number'
-    }
-    />),
-    onHeaderCell: () => onHeaderCellClick('contact_number'),
-    dataIndex: 'contact_number',
-    key: 'contact_number',
-    width: 200,
-    render: (value: string) => (
-      <>
-      {value && value != "" ? <Text className="font-medium text-gray-700">{value}</Text> : <Text className="font-medium text-gray-700">-</Text>}
-      </>
-    ),
-  },
-  {
-    title: (
-    <HeaderCell
-    title="Email ID"
-    sortable
-    ascending={
-      sortConfig?.direction === 'asc' && sortConfig?.key === 'email'
-    }
-    />),
-    onHeaderCell: () => onHeaderCellClick('email'),
-    dataIndex: 'email',
-    key: 'email',
-    width: 200,
-    render: (_: any, row: any) => (
-      <Text className="font-medium text-gray-700">{row.email}</Text>
-    ),
-  },
-  {
-    title: (
-    <HeaderCell
-    title="Permission"
-    sortable
-    ascending={
-      sortConfig?.direction === 'asc' && sortConfig?.key === 'role'
-    }
-    />),
-    onHeaderCell: () => onHeaderCellClick('role'),
-    dataIndex: 'reference_id',
-    key: 'reference_id',
-    width: 200,
-    render: (value: Record<string, any>) => getRoleName(value?.role?.name),
-  },
-  {
-    title: (
-    <HeaderCell
-    title="Status"
-    sortable
-    ascending={
-      sortConfig?.direction === 'asc' && sortConfig?.key === 'status'
-    }
-    />),
-    onHeaderCell: () => onHeaderCellClick('status'),
-    dataIndex: 'status',
-    key: 'status',
-    width: 200,
-    render: (value: string) => getStatusBadge(value),
-  },
-  {
-    title: (
-      <HeaderCell
-        title="Created"
-        sortable
-        ascending={
-          sortConfig?.direction === 'asc' && sortConfig?.key === 'createdAt'
-        }
-      />
-    ),
-    onHeaderCell: () => onHeaderCellClick('createdAt'),
-    dataIndex: 'createdAt',
-    key: 'createdAt',
-    width: 200,
-    render: (value: string) => {
-      const date = moment(value).fromNow();
-      return <Text className="font-medium text-gray-700">{date}</Text>
+}: Columns) => {
+
+  const token = localStorage.getItem('token')
+  const router = useRouter()
+  const dispatch = useDispatch()
+
+  const paginationParams = useSelector((state: any) => state?.root?.teamMember?.paginationParams);
+  console.log(paginationParams, 'paginationParams')
+
+  const ClintteamlistAPIcall = async () => {
+    let { page, items_per_page, sort_field, sort_order, search } = paginationParams;
+    await dispatch(getAllTeamMember({ page, items_per_page, sort_field, sort_order, search, pagination: true }));
+
+  }
+
+  return [
+    {
+      title: (
+        <div className="ps-3.5">
+          <Checkbox
+            title={'Select All'}
+            onChange={handleSelectAll}
+            checked={checkedItems.length === data.length}
+            className="cursor-pointer"
+          />
+        </div>
+      ),
+      dataIndex: 'checked',
+      key: 'checked',
+      width: 50,
+      render: (_: any, row: any) => (
+        <div className="inline-flex ps-3.5">
+          <Checkbox
+            className="cursor-pointer"
+            checked={checkedItems.includes(row._id)}
+            {...(onChecked && { onChange: () => onChecked(row._id) })}
+          />
+        </div>
+      ),
     },
-  },
-  {
-    // Need to avoid this issue -> <td> elements in a large <table> do not have table headers.
-    title: <HeaderCell title="Actions" className="opacity-0" />,
-    dataIndex: 'action',
-    key: 'action',
-    width: 120,
-    render: (_: string, row: TeamMemberType) => (
-      <div className="flex items-center justify-end gap-3 pe-4">
-      
-        <CustomModalButton 
-          icon={<PencilIcon className="h-4 w-4" />}
-          view={<AddTeamMemberForm title="Edit Team member" row={row}/>}
-          customSize="625px"
-          title='Edit Team member'
+    {
+      title: (
+        <HeaderCell
+          title="Name"
+          sortable
+          ascending={
+            sortConfig?.direction === 'asc' && sortConfig?.key === 'name'
+          }
+        />),
+      onHeaderCell: () => onHeaderCellClick('name'),
+      dataIndex: 'name',
+      key: 'name',
+      width: 200,
+      render: (value: string) => (
+        <Text className="font-medium text-gray-700">{value}</Text>
+      ),
+    },
+    {
+      title: (
+        <HeaderCell
+          title="Mobile Number"
+          sortable
+          ascending={
+            sortConfig?.direction === 'asc' && sortConfig?.key === 'contact_number'
+          }
+        />),
+      onHeaderCell: () => onHeaderCellClick('contact_number'),
+      dataIndex: 'contact_number',
+      key: 'contact_number',
+      width: 200,
+      render: (value: string) => (
+        <>
+          {value && value != "" ? <Text className="font-medium text-gray-700">{value}</Text> : <Text className="font-medium text-gray-700">-</Text>}
+        </>
+      ),
+    },
+    {
+      title: (
+        <HeaderCell
+          title="Email ID"
+          sortable
+          ascending={
+            sortConfig?.direction === 'asc' && sortConfig?.key === 'email'
+          }
+        />),
+      onHeaderCell: () => onHeaderCellClick('email'),
+      dataIndex: 'email',
+      key: 'email',
+      width: 200,
+      render: (_: any, row: any) => (
+        <Text className="font-medium text-gray-700">{row.email}</Text>
+      ),
+    },
+    {
+      title: (
+        <HeaderCell
+          title="Permission"
+          sortable
+          ascending={
+            sortConfig?.direction === 'asc' && sortConfig?.key === 'role'
+          }
+        />),
+      onHeaderCell: () => onHeaderCellClick('role'),
+      dataIndex: 'reference_id',
+      key: 'reference_id',
+      width: 200,
+      render: (value: Record<string, any>) => getRoleName(value?.role?.name),
+    },
+    {
+      title: (
+        <HeaderCell
+          title="Status"
+          sortable
+          ascending={
+            sortConfig?.direction === 'asc' && sortConfig?.key === 'status'
+          }
+        />),
+      onHeaderCell: () => onHeaderCellClick('status'),
+      dataIndex: 'status',
+      key: 'status',
+      width: 200,
+      render: (value: string) => getStatusBadge((value && value === "payment_pending" ? "Payment Pending" : value)),
+    },
+    {
+      title: (
+        <HeaderCell
+          title="Created"
+          sortable
+          ascending={
+            sortConfig?.direction === 'asc' && sortConfig?.key === 'createdAt'
+          }
         />
-        <Tooltip
-          size="sm"
-          content={() => 'View Team member'}
-          placement="top"
-          color="invert"
-        >
-          {/* <Link href={routes.viewTeam}> */}
-            <Button size="sm" variant="outline" className='bg-white text-black' aria-label={'View Member'}>
-              <EyeIcon className="h-4 w-4" />
-            </Button>
-          {/* </Link> */}
-        </Tooltip>
-        <DeletePopover
-          title={`Delete the Team member`}
-          description={`Are you sure you want to delete?`}
-          onDelete={() => onDeleteItem(row._id, currentPage, pageSize, data?.length <= 1 ? true : false, sortConfig, searchTerm)}
-        />
-      </div>
-    ),
-  },
-];
+      ),
+      onHeaderCell: () => onHeaderCellClick('createdAt'),
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      width: 200,
+      render: (value: string) => {
+        const date = moment(value).fromNow();
+        return <Text className="font-medium text-gray-700">{date}</Text>
+      },
+    },
+    {
+      // Need to avoid this issue -> <td> elements in a large <table> do not have table headers.
+      title: <HeaderCell title="Actions" className="opacity-0" />,
+      dataIndex: 'action',
+      key: 'action',
+      width: 120,
+      render: (_: string, row: any) => (
+        <>
+          {row?.status === "payment_pending" ? <div> <Button className='w-full' onClick={() => { initiateRazorpay(router, routes.agency_team, token, row?.reference_id?._id, ClintteamlistAPIcall) }}>Pay</Button></div> :
+
+            <div className="flex items-center justify-end gap-3 pe-4">
+
+              <CustomModalButton
+                icon={<PencilIcon className="h-4 w-4" />}
+                view={<AddTeamMemberForm title="Edit Team member" row={row} />}
+                customSize="625px"
+                title='Edit Team member'
+              />
+              <Tooltip
+                size="sm"
+                content={() => 'View Team member'}
+                placement="top"
+                color="invert"
+              >
+                {/* <Link href={routes.viewTeam}> */}
+                <Button size="sm" variant="outline" className='bg-white text-black' aria-label={'View Member'}>
+                  <EyeIcon className="h-4 w-4" />
+                </Button>
+                {/* </Link> */}
+              </Tooltip>
+              <DeletePopover
+                title={`Delete the Team member`}
+                description={`Are you sure you want to delete?`}
+                onDelete={() => onDeleteItem(row._id, currentPage, pageSize, data?.length <= 1 ? true : false, sortConfig, searchTerm)}
+              />
+            </div>}
+
+        </>
+      ),
+    },
+  ];
+}
