3522cb3949ffe430cff42442c3428e0c7b411453
BhautikZ <bhautikztridhyatech@gmail.com>
2024-02-16 18:12:24 +0530

    -add confirmation poup

--- src/app/(hydrogen)/agency-team/main-page.tsx
+++ src/app/(hydrogen)/agency-team/main-page.tsx
@@ -7,53 +7,106 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import CustomTable from '@/components/common-tables/table';
 import { GetclientTeamColumns } from '@/app/shared/(user)/agency/agency-team/team-list/columns';
-import { deleteTeamMember, getAllTeamMember, setPagginationParams } from '@/redux/slices/user/team-member/teamSlice';
+import {
+  deleteTeamMember,
+  getAllTeamMember,
+  setPagginationParams,
+} from '@/redux/slices/user/team-member/teamSlice';
 import { PiPlusBold } from 'react-icons/pi';
-
+import { useModal } from '@/app/shared/modal-views/use-modal';
+import DeleteModel from './confirmationPopup';
 
 const pageHeader = {
   title: 'Agency Team',
 };
 
-
 export default function TeamDataTablePage() {
   const dispatch = useDispatch();
   const [pageSize, setPageSize] = useState(5);
+  const { closeModal, openModal } = useModal();
   const teamMemberData = useSelector((state: any) => state?.root?.teamMember);
   // const clientSliceData = useSelector((state: any) => state?.root?.client);
 
-
   const handleChangePage = async (paginationParams: any) => {
-    let { page, items_per_page, sort_field, sort_order, search } = paginationParams;
+    let { page, items_per_page, sort_field, sort_order, search } =
+      paginationParams;
 
-    await dispatch(setPagginationParams(paginationParams))
-    const response = await dispatch(getAllTeamMember({ page, items_per_page, sort_field, sort_order, search, pagination: true }));
+    await dispatch(setPagginationParams(paginationParams));
+    const response = await dispatch(
+      getAllTeamMember({
+        page,
+        items_per_page,
+        sort_field,
+        sort_order,
+        search,
+        pagination: true,
+      })
+    );
     const { data } = response?.payload;
     const maxPage: number = data?.page_count;
 
     if (page > maxPage) {
       page = maxPage > 0 ? maxPage : 1;
-      await dispatch(getAllTeamMember({ page, items_per_page, sort_field, sort_order, search, pagination: true }));
+      await dispatch(
+        getAllTeamMember({
+          page,
+          items_per_page,
+          sort_field,
+          sort_order,
+          search,
+          pagination: true,
+        })
+      );
       return data?.teamMemberList;
     }
     if (data && data?.teamMemberList && data?.teamMemberList.length !== 0) {
-      return data?.teamMemberList
+      return data?.teamMemberList;
     }
   };
 
-  const handleDeleteById = async (id: string | string[], currentPage?: any, countPerPage?: number, sortConfig?: Record<string, string>, searchTerm?: string) => {
-
+  const handleDeleteById = async (
+    id: string | string[],
+    currentPage?: any,
+    countPerPage?: number,
+    sortConfig?: Record<string, string>,
+    searchTerm?: string
+  ) => {
     try {
+      console.log('delete');
       const res = await dispatch(deleteTeamMember({ teamMemberIds: id }));
       if (res.payload.success === true) {
-        const reponse = await dispatch(getAllTeamMember({ page: currentPage, items_per_page: countPerPage, sort_field: sortConfig?.key, sort_order: sortConfig?.direction, search: searchTerm, pagination: true }));
+        const reponse = await dispatch(
+          getAllTeamMember({
+            page: currentPage,
+            items_per_page: countPerPage,
+            sort_field: sortConfig?.key,
+            sort_order: sortConfig?.direction,
+            search: searchTerm,
+            pagination: true,
+          })
+        );
+        if (res?.payload?.data?.force_fully_remove === true) {
+          openModal({
+            view: (
+              <DeleteModel
+                forsfully={res?.payload?.data?.force_fully_remove}
+                id={id}
+                page={currentPage}
+                items_per_page={countPerPage}
+                sort_field={sortConfig?.key}
+                sort_order={sortConfig?.direction}
+                search={searchTerm}
+                pagination={true}
+              />
+            ),
+            customSize: '500px',
+          });
+        }
       }
     } catch (error) {
       console.error(error);
     }
   };
-
-
 
   return (
     <>
---
+++ src/app/(hydrogen)/client/confirmationpouclient.tsx
@@ -0,0 +1,100 @@
+'use client';
+import { useModal } from '@/app/shared/modal-views/use-modal';
+import { CalendarEvent } from '@/types';
+import { PiMapPin, PiTrashFill, PiXBold } from 'react-icons/pi';
+import { ActionIcon, Button, Text, Title } from 'rizzui';
+import { MdOutlineCalendarMonth } from 'react-icons/md';
+import cn from '@/utils/class-names';
+import {
+  RemoveUsersub,
+  getAllSeats,
+} from '@/redux/slices/user/manage-subscription.tsx/SubscriptionSlice';
+import { useDispatch } from 'react-redux';
+import {
+  deleteTeamMember,
+  getAllTeamMember,
+} from '@/redux/slices/user/team-member/teamSlice';
+import {
+  deleteClient,
+  getAllClient,
+} from '@/redux/slices/user/client/clientSlice';
+
+function DeleteModel({
+  forsfully,
+  id,
+  page,
+  items_per_page,
+  sort_field,
+  sort_order,
+  search,
+  pagination,
+  client_id,
+}: any) {
+  const { openModal, closeModal } = useModal();
+  const disptach = useDispatch();
+
+  const handleDelete = async () => {
+    const res = await disptach(
+      deleteClient({ client_ids: id, force_fully_remove: forsfully })
+    );
+    if (res.payload.success === true) {
+      closeModal();
+      const reponse = await disptach(
+        getAllClient({
+          page: page,
+          items_per_page: items_per_page,
+          sort_field: sort_field,
+          sort_order: sort_order,
+          search: search,
+          pagination: true,
+        })
+      );
+    }
+  };
+
+  function handelCAncle() {
+    closeModal();
+  }
+
+  return (
+    <div className="m-auto p-4 md:px-7 md:pb-10 md:pt-6">
+      <div className="mb-6 flex flex-col items-center justify-between">
+        <Title
+          as="h6"
+          className="mb-0.5 flex items-start text-sm text-gray-700 sm:items-center"
+        >
+          <PiTrashFill className="me-1 h-[17px] w-[17px]" />
+          This User have been assigned with Task(s) which are still pending.
+        </Title>
+        <Text className="mb-2 leading-relaxed text-gray-500">
+          Are you sure you want to delete?
+        </Text>
+        {/* <ActionIcon
+          size="sm"
+          variant="text"
+          onClick={() => closeModal()}
+          className="p-0 text-gray-500 hover:!text-gray-900"
+        >
+          <PiXBold className="h-[18px] w-[18px]" />
+        </ActionIcon> */}
+      </div>
+      <div className={cn('grid grid-cols-2 gap-4 pt-5 ')}>
+        <Button
+          className="hover:bg-gray-700 dark:bg-gray-200 dark:text-white dark:hover:bg-gray-300 dark:active:bg-gray-100"
+          onClick={handleDelete}
+        >
+          Yes
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => handelCAncle()}
+          className="dark:hover:border-gray-400"
+        >
+          No
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteModel;
